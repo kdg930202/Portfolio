0fce50d550a1171c4429f1c06e8db7f4b8e82689
kdg930202 <52188333+kdg930202@users.noreply.github.com>
2021-04-14 11:43:17 +0900

    Navbar toggle button

--- main.js
+++ main.js
@@ -21,7 +21,78 @@ navbarMenu.addEventListener('click', (event) => {
   if (link == null) {
     return;
   }
+  navbarMenu.classList.remove('open');
   console.log(event.target.dataset.link);
   const scrollTo = document.querySelector(link);
   scrollTo.scrollIntoView({behavior: 'smooth'})
 })
+
+
+
+const navbarToggleBtn = document.querySelector('.navbar__toggle_btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+});
+
+
+
+const homeContactBtn = document.querySelector('.home__contact')
+homeContactBtn.addEventListener('click', (event) => {
+  const scrollTo = document.querySelector('#contact');
+  scrollTo.scrollIntoView({behavior: 'smooth'})
+
+})
+
+
+//Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll',(event)=>{
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+})
+
+// Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector('.arrow-up')
+document.addEventListener('scroll', (event) => {
+  if (window.scrollY > homeHeight/2) {
+    arrowUp.classList.add('visible')
+  }else {
+    arrowUp.classList.remove('visible')
+  }
+})
+
+arrowUp.addEventListener('click', (event)=>{
+  const scrollTo = document.querySelector('#home');
+  scrollTo.scrollIntoView({behavior: 'smooth'})
+})
+
+
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+
+  // Remove selection from the previous item and select the new one
+  const active = document.querySelector('.category__btn.selected');
+  if (active != null) {
+    active.classList.remove('selected');
+  }
+  e.target.classList.add('selected');
+
+  projectContainer.classList.add('anim-out');
+  setTimeout(() => {
+    projects.forEach((project) => {
+      console.log(project.dataset.type);
+      if (filter === '*' || filter === project.dataset.type) {
+        project.classList.remove('invisible');
+      } else {
+        project.classList.add('invisible');
+      }
+    });
+    projectContainer.classList.remove('anim-out');
+  }, 300);
+});
